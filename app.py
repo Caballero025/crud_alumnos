3cc564bc5ae39c1b7929ea1b80cc0b8688edbc03
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-02-11 15:42:23 -0600

    Actualisacion de app

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hola_mundo():
     return 'Hola Mundo'
+@app.route('/alumnos')
+def hola_mundo():
+    return 'Aqui van los alumnos'
 
 if __name__ == '__main__':
     app.run(debug=True)
