4f97bee4a85807fb97774ac6b3986e0d792b671d
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-02-11 18:11:51 -0600

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask
 #Crear instancia
 app = Flask(__name__)
 
-#Ruta raiz
+
 @app.route('/')
-def hola_mundo():
-    return 'Hola Mundo'
-@app.route('/alumnos')
 def getAlumnos():
     return 'Aqui van los alumnos'
 
